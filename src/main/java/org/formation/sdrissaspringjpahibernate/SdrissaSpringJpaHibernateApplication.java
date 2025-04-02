334f1719d357622d365ae1859c77532124da924e
drsanou <drisanou@bceao.int>
2025-04-02 12:34:18 +0000

    first commit

---
+++ src/main/java/org/formation/sdrissaspringjpahibernate/SdrissaSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package org.formation.sdrissaspringjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdrissaSpringJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdrissaSpringJpaHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/formation/sdrissaspringjpahibernate/SdrissaSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.sdrissaspringjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdrissaSpringJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
